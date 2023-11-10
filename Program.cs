b09210d6ddf356732e0b34d7df2c0cd7169283c7
Armando <jarodriguezvera@gmail.com>
2023-11-09 22:08:16 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BusquedaBinaria_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 20, 30, 42, 52, 72, 100 };
+            int X = 30;
+            int pos = BusquedaBinaria(A, 0, A.Length, X);
+            Console.WriteLine("El valor {0} está en : {1}", X, pos);
+
+            Console.ReadLine();
+
+
+        }
+
+
+        static int BusquedaBinaria(int[] A, int IZQ, int DER, int X)
+        {
+
+            if ( DER <= IZQ)
+            {
+                return -1;
+            }
+            else
+            {
+                int CEN = (IZQ + DER) / 2;
+
+                if ( A[CEN] ==  X )
+                {
+                    return CEN;
+                }
+                else if( X < A[CEN] )
+                {
+                    return BusquedaBinaria(A, IZQ, CEN,X);
+                }
+                else
+                {
+                    return BusquedaBinaria(A, CEN , DER, X);
+                }
+            }
+
+        }
+    }
+}
